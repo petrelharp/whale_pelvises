2f82e19cc62c87f9e89d8e0a6d9af9270deed25c
peter <petrel.harp@gmail.com>
2013-08-25 01:07:33 -0700

    .

--- shape-analysis.R
+++ shape-analysis.R
@@ -17,14 +17,15 @@ estpar <- unlist( pars["estpar",] )
 
 # shapes
 shapediff <- read.table("61_pairwise_shapes.out", header=TRUE)
-shapediff <- subset( shapediff, ( bone1==bone2 ) & ( specimen1 %in% rownames(thedata) ) & ( specimen2 %in% rownames(thedata) ) )
-shapediff$species1 <- whales$species[match(shapediff$specimen1,whales$specimen)]
-shapediff$species2 <- whales$species[match(shapediff$specimen2,whales$specimen)]
 ### TEMPORARY: exclude problem sample
 shapediff <- subset( shapediff, (specimen1 != "USNM_504107") & (specimen2 != "USNM_504107")  )
 ####
 shapediff$specimen1 <- factor( shapediff$specimen1, levels=rownames(thedata) )
 shapediff$specimen2 <- factor( shapediff$specimen2, levels=rownames(thedata) )
+shapediff$species1 <- whales$species[match(shapediff$specimen1,whales$specimen)]
+shapediff$species2 <- whales$species[match(shapediff$specimen2,whales$specimen)]
+# restrict to same-bone comparisons and ones observed in the data
+shapediff <- subset( shapediff, ( bone1==bone2 ) & ( specimen1 %in% rownames(thedata) ) & ( specimen2 %in% rownames(thedata) ) )
 
 ## sample-by-sample
 # matrix of pelvis shape differences: exclude within-individual comparisons
@@ -38,24 +39,27 @@ ribdiff[ cbind(as.numeric(shapediff$specimen1)[ribs],as.numeric(shapediff$specim
 
 ###
 # species-by-species
+# rib
 rib.speciesdiff <- with( subset(shapediff, bone1==bone2 & bone1=="rib" & ( specimen1 != specimen2 ) ), 
-        tapply( shape_difference, list(species1,species2), sum, na.rm=TRUE ) 
-            + tapply( shape_difference, list(species2,species1), sum, na.rm=TRUE ) 
-        )
+        tapply( shape_difference, list(species1,species2), sum, na.rm=TRUE ) )
+rib.speciesdiff[is.na(rib.speciesdiff)] <- 0
+rib.speciesdiff <- rib.speciesdiff + t(rib.speciesdiff) - diag(diag(rib.speciesdiff))
 rib.denom <- with( subset(shapediff, bone1==bone2 & bone1=="rib" & ( specimen1 != specimen2 ) ), 
-        tapply( shape_difference, list(species1,species2), length )
-            +  tapply( shape_difference, list(species2,species1), length  ) 
-        )
+        tapply( shape_difference, list(species1,species2), length ) )
+rib.denom[is.na(rib.denom)] <- 0
+rib.denom <- rib.denom + t(rib.denom) - diag(diag(rib.denom))
 rib.speciesdiff <- rib.speciesdiff / rib.denom
+# pelvis
 pelvic.speciesdiff <- with( subset(shapediff, bone1==bone2 & bone1=="pelvic" & ( specimen1 != specimen2 ) ), 
-        tapply( shape_difference, list(species1,species2), sum, na.rm=TRUE ) 
-            + tapply( shape_difference, list(species2,species1), sum, na.rm=TRUE ) 
-        )
+        tapply( shape_difference, list(species1,species2), sum, na.rm=TRUE ) )
+pelvic.speciesdiff[is.na(pelvic.speciesdiff)] <- 0
+pelvic.speciesdiff <- pelvic.speciesdiff + t(pelvic.speciesdiff) - diag(diag(pelvic.speciesdiff))
 pelvic.denom <- with( subset(shapediff, bone1==bone2 & bone1=="pelvic" & ( specimen1 != specimen2 ) ), 
-        tapply( shape_difference, list(species1,species2), length )
-            +  tapply( shape_difference, list(species2,species1), length  ) 
-        )
+        tapply( shape_difference, list(species1,species2), length ) )
+pelvic.denom[is.na(pelvic.denom)] <- 0
+pelvic.denom <- pelvic.denom + t(pelvic.denom) - diag(diag(pelvic.denom))
 pelvic.speciesdiff <- pelvic.speciesdiff / pelvic.denom
+
 # treedist
 species.order <- match( levels(shapediff$species1), species.tree$tip.label  )
 species.treedist <- treedist( species.tree )[ species.order, species.order ]
@@ -77,7 +81,7 @@ lengthmean[!islength] <- fullmat[!islength,islength] %*% solve( fullmat[islength
 dimnames(lengthmean) <- dimnames(thedata)
 lengthmean <- sweep( lengthmean, 2, unlist(phylomeans), "+" )
 # just the testes
-fullmean <- cbind( fullmean, scaled.testes=fullmean[,"actual_testes_mass_max"] - lengthmean[,"actual_testes_mass_max"] )
+fullmean <- cbind( fullmean, scaled.testes=(fullmean[,"actual_testes_mass_max"] - lengthmean[,"actual_testes_mass_max"]) )
 # plot( lengthmean[,"actual_testes_mass_max"], fullmean[,'scaled.testes'] ) ## looks good
 # associate mean value of adjacent nodes to each edge
 edge.values <- t( apply( tree$edge, 1, function (kk) { colMeans(fullmean[kk,]) } ) )
@@ -92,8 +96,10 @@ testes.spdist <- testes.treedist[match(levels(shapediff$species1),rownames(theda
 # visualization
 adjtree <- tree
 adjtree$edge.length[tip.edges] <- (.05/3.16)^2  # reasonable value from initial-values.R
-plot(adjtree,edge.width=edge.testes-min(edge.testes))
-plot(adjtree,edge.width=fullmean[edge.indices,'bodylength'])
+#
+layout(t(1:2))
+plot(adjtree,edge.color=diverge_hcl(64)[cut(edge.testes,breaks=seq((-1)*max(abs(edge.testes)),max(abs(edge.testes)),length.out=65))],main='relative testes size',edge.width=3,tip.color=ifelse(is.na(thedata[,"left.rib"])&is.na(thedata[,"right.rib"]),'red','black')[1:Ntip(adjtree)],cex=.5)
+plot(adjtree,edge.color=diverge_hsv(64)[cut(fullmean[edge.indices,'bodylength'],64)],main='body length',edge.width=3,cex=.5,tip.color=ifelse(is.na(thedata[,"left.rib"])&is.na(thedata[,"right.rib"]),'red','black')[1:Ntip(adjtree)])
 
 
 ######
@@ -119,38 +125,71 @@ sub.resid.pelvic.lm <- lm( resid(sub.pelvic.lm) ~  testes.spdist[upper.tri(teste
 
 # look at these:
 pdf( file="ribs-correlate.pdf", width=8, height=5, pointsize=10 )
-layout(matrix(1:6,nrow=2))
+layout(matrix(1:4,nrow=2))
 cols <- ifelse(have.both,"black","red")
+##
 # shape-tree-diffs
 plot( species.treedist[upper.tri(species.treedist)], rib.speciesdiff[upper.tri(species.treedist)], xlab="tree distance", ylab="shape difference", main="rib", col=cols[have.ribs], xlim=range(species.treedist) )
 abline(coef(rib.lm),col='red')
 abline(coef(sub.rib.lm))
 plot( species.treedist[upper.tri(species.treedist)], pelvic.speciesdiff[upper.tri(species.treedist)], xlab="tree distance", ylab="shape difference", main="pelvic", col=cols[have.pelvics], xlim=range(species.treedist) )
+identify( species.treedist[upper.tri(species.treedist)], pelvic.speciesdiff[upper.tri(species.treedist)], labels=outer(rownames(pelvic.speciesdiff),colnames(pelvic.speciesdiff),paste)[upper.tri(species.treedist)] )
 abline(coef(pelvic.lm),col='red')
 abline(coef(sub.pelvic.lm))
+# shape-testes-tree-diffs
+plot( testes.spdist[upper.tri(testes.spdist)], rib.speciesdiff[upper.tri(testes.spdist)], xlab="testes-weighted tree distance", ylab="shape difference", main="rib", col=cols[have.ribs], xlim=range(testes.spdist) )
+plot( testes.spdist[upper.tri(testes.spdist)], pelvic.speciesdiff[upper.tri(testes.spdist)], xlab="testes-weighted tree distance", ylab="shape difference", main="pelvic", col=cols[have.pelvics], xlim=range(testes.spdist) )
+##
 # resids-testes-distance
-plot( testes.spdist[upper.tri(testes.spdist)][have.ribs], resid(rib.lm), xlab="relative testes-weighted tree distance", ylab="residuals of bone distance accounting for tree distance", main="ribs", col=cols[have.ribs], xlim=range( testes.spdist ) )
+plot( testes.spdist[upper.tri(testes.spdist)][have.ribs], resid(rib.lm), xlab="testes-weighted tree distance", ylab="residuals of bone distance accounting for tree distance", main="ribs", col=cols[have.ribs], xlim=range( testes.spdist ) )
 abline(coef(resid.rib.lm),col='red')
-plot( testes.spdist[upper.tri(testes.spdist)][have.pelvics], resid(pelvic.lm), xlab="relative testes-weighted tree distance", ylab="residuals of bone distance accounting for tree distance", main="pelvic, residuals of all data", col=cols[have.pelvics], xlim=range( testes.spdist ) )
+plot( testes.spdist[upper.tri(testes.spdist)][have.pelvics], resid(pelvic.lm), xlab="testes-weighted tree distance", ylab="residuals of bone distance accounting for tree distance", main="pelvic, residuals of all data", col=cols[have.pelvics], xlim=range( testes.spdist ) )
 abline(coef(resid.pelvic.lm),col='red')
 # resids-testes-distance, all observations
-plot( testes.spdist[upper.tri(testes.spdist)][have.both], resid(sub.rib.lm), xlab="relative testes-weighted tree distance", ylab="residuals of bone distance accounting for tree distance", main="ribs", col=cols[have.both], xlim=range( testes.spdist ) )
+plot( testes.spdist[upper.tri(testes.spdist)][have.both], resid(sub.rib.lm), xlab="testes-weighted tree distance", ylab="residuals of bone distance accounting for tree distance", main="ribs", col=cols[have.both], xlim=range( testes.spdist ) )
 abline(coef(sub.resid.rib.lm))
-plot( testes.spdist[upper.tri(testes.spdist)][have.both], resid(sub.pelvic.lm), xlab="relative testes-weighted tree distance", ylab="residuals of bone distance accounting for tree distance", main="pelvic, residuals for complete obs", col=cols[have.both], xlim=range( testes.spdist ) )
+plot( testes.spdist[upper.tri(testes.spdist)][have.both], resid(sub.pelvic.lm), xlab="testes-weighted tree distance", ylab="residuals of bone distance accounting for tree distance", main="pelvic, residuals for complete obs", col=cols[have.both], xlim=range( testes.spdist ) )
 abline(coef(sub.resid.pelvic.lm))
 dev.off()
-### LOOKS GOOD!!
+### Hm.  Looks good?
 
 
 
+#####
+# quick-and-dirty parameters
+# within-species variance
+tip.ribvar <- with( subset( shapediff, species1==species2 & bone1=="rib" & bone2=="rib"), tapply( shape_difference, species1, mean, na.rm=TRUE ) )
+tip.pelvicvar <- with( subset( shapediff, species1==species2 & bone1=="pelvic" & bone2=="pelvic"), tapply( shape_difference, species1, mean, na.rm=TRUE ) )
+# estimate of xi2P,R
+est.xi2P <- with( subset( shapediff, species1==species2 & bone1=="pelvic" & bone2=="pelvic"), var( shape_difference, na.rm=TRUE ) / mean( shape_difference, na.rm=TRUE ) ) / 2
+est.xi2R <- with( subset( shapediff, species1==species2 & bone1=="rib" & bone2=="rib"), var( shape_difference, na.rm=TRUE ) / mean( shape_difference, na.rm=TRUE ) ) / 2
+# estimate of ksP,R
+ksP <- with( subset( shapediff, species1==species2 & bone1=="pelvic" & bone2=="pelvic"), 2 * mean( shape_difference, na.rm=TRUE )^2 / var( shape_difference, na.rm=TRUE ) )
+ksR <- with( subset( shapediff, species1==species2 & bone1=="rib" & bone2=="rib"), 2 * mean( shape_difference, na.rm=TRUE )^2 / var( shape_difference, na.rm=TRUE ) )
 
+# check:
+tmp <- with( subset( shapediff, species1==species2 & bone1=="pelvic" & bone2=="pelvic"), hist( shape_difference, main="pelvic", breaks=40 ) )
+lines( tmp$mids, sum(tmp$counts) * diff(pchisq( tmp$breaks/est.xi2P, df=ksP )) )
+tmp <- with( subset( shapediff, species1==species2 & bone1=="rib" & bone2=="rib"), hist( shape_difference, main='rib', breaks=40 ) )
+lines( tmp$mids, sum(tmp$counts) * diff(pchisq( tmp$breaks/est.xi2R, df=ksR )) )
+lines( tmp$mids, sum(tmp$counts) * diff(pchisq( tmp$breaks/est.xi2R*(ksP/ksR), df=ksP )), col='red' )  # looks better
 
-####
-# Pseudolikelihood:
+# estimates
+est.ks <- ksP
+est.xi2R <- est.xi2R * ksP/ksR
 
+# sigma2S : slope of relation between shape diff and tree dist, divided by ks.
+est.sigma2S <- (coef(pelvic.lm)/ksP)[2]
+# gammaP : slope of relation to residual
+est.gammaP <- (coef(resid.pelvic.lm)/ksP)[2]
 
-###
-# include correlations?
+initpar <- c( 
+        ks = est.ks,
+        sigma2S = est.sigma2S,
+        gammaP = est.gammaP,
+        xi2P = est.xi2P
+    )
+names(initpar) <- c('ks','sigma2S','gammaP','xi2P')
 
 ####
 # update branch lengths function
@@ -163,37 +202,55 @@ tip.lengths[ - tip.edges ] <- 0
 
 # given parameters get rescaled branch lengths
 scale.brlens <- function (par) {
-    # par = ks, sigma2S, gammaP, xi2P
-    ks <- par[1]
-    sigma2S <- par[2]
-    gammaP <- par[3]
-    xi2P <- par[4]
+    # par = sigma2S, gammaP, xi2P
+    sigma2S <- par[1]
+    gammaP <- par[2]
+    xi2P <- par[3]
     return( internal.lengths * ( sigma2S + gammaP * edge.testes ) + tip.lengths * xi2P )
 }
 
-treemat <- treedist( adjtree, descendants=descendants )
+treemat <- treedist( adjtree, edge.length=scale.brlens(initpar[2:4]), descendants=descendants )
 
-# quick-and-dirty parameters
-# within-species variance
-tip.ribvar <- with( subset( shapediff, species1==species2 & bone1=="rib" & bone2=="rib"), tapply( shape_difference, species1, mean, na.rm=TRUE ) )
-tip.pelvicvar <- with( subset( shapediff, species1==species2 & bone1=="pelvic" & bone2=="pelvic"), tapply( shape_difference, species1, mean, na.rm=TRUE ) )
-# estimate of xi2P,R
-xi2P <- with( subset( shapediff, species1==species2 & bone1=="pelvic" & bone2=="pelvic"), var( shape_difference, na.rm=TRUE ) / mean( shape_difference, na.rm=TRUE ) ) / 2
-xi2R <- with( subset( shapediff, species1==species2 & bone1=="rib" & bone2=="rib"), var( shape_difference, na.rm=TRUE ) / mean( shape_difference, na.rm=TRUE ) ) / 2
-# estimate of ksP,R
-ksP <- with( subset( shapediff, species1==species2 & bone1=="pelvic" & bone2=="pelvic"), 2 * mean( shape_difference, na.rm=TRUE )^2 / var( shape_difference, na.rm=TRUE ) )
-ksR <- with( subset( shapediff, species1==species2 & bone1=="rib" & bone2=="rib"), 2 * mean( shape_difference, na.rm=TRUE )^2 / var( shape_difference, na.rm=TRUE ) )
-# check:
-tmp <- with( subset( shapediff, species1==species2 & bone1=="pelvic" & bone2=="pelvic"), hist( shape_difference, main="pelvic", breaks=40 ) )
-lines( tmp$mids, sum(tmp$counts) * diff(pchisq( tmp$breaks/xi2P, df=ksP )) )
-tmp <- with( subset( shapediff, species1==species2 & bone1=="rib" & bone2=="rib"), hist( shape_difference, main='rib', breaks=40 ) )
-lines( tmp$mids, sum(tmp$counts) * diff(pchisq( tmp$breaks/xi2R, df=ksR )) )
-lines( tmp$mids, sum(tmp$counts) * diff(pchisq( tmp$breaks/xi2R*(ksP/ksR), df=ksP )), col='red' )  # looks better
-# estimates
-ks <- ksP
-xi2P <- xi2P
-xi2R <- xi2R * ksP/ksR
+# moment estimate?
+have.dpelvic <- !is.na(pelvicdiff)
+datavec <- pelvicdiff[have.dpelvic] / initpar[1]
+f1 <- function (spar) {
+    sigma2S <- spar[1]
+    gammaP <- spar[2]
+    xi2P <- spar[3]
+    elens <- ( internal.lengths * ( sigma2S + gammaP * edge.testes ) + tip.lengths * xi2P )
+    treemat <- treedist( adjtree, edge.length=elens, descendants=descendants )
+    return( sum( ( datavec - treemat[have.dpelvic] )^2 ) )
+}
 
+est.vals <- optim( par=initpar[2:4], fn=f1, method="BFGS", control=list(fnscale=1e4,maxit=10) )
+est.vals <- optim( par=est.vals$par, fn=f1, method="BFGS", control=list(fnscale=1e4,maxit=10) )
+
+new.treemat <- treedist( adjtree, edge.length=scale.brlens(est.vals$par), descendants=descendants )
+
+layout(t(1:2))
+plot( treemat[have.dpelvic], datavec )
+abline(0,1)
+plot( new.treemat[have.dpelvic], datavec )
+abline(0,1)
+
+
+# pseudolikelihood
+have.dpelvic <- !is.na(pelvicdiff)
+chisq.datavec <- pelvicdiff[have.dpelvic]
+f2 <- function (spar) {
+    sigma2S <- spar[1]
+    gammaP <- spar[2]
+    xi2P <- spar[3]
+    elens <- ( internal.lengths * ( sigma2S + gammaP * edge.testes ) + tip.lengths * xi2P )
+    treemat <- treedist( adjtree, edge.length=elens, descendants=descendants )
+    return( (-1)*sum( dchisq( chisq.datavec/treemat[have.dpelvic], initpar[1], log=TRUE ) ) )
+}
+
+est.vals <- optim( par=initpar[2:4], fn=f2, method="BFGS", control=list(fnscale=1e4,maxit=10) )
+est.vals <- optim( par=est.vals$par, fn=f2, method="BFGS", control=list(fnscale=1e4,maxit=10) )
+
+########
 # add tips
 rib.lengths <- tree$edge.length
 rib.lengths[ tip.edges ] <- xi2R
