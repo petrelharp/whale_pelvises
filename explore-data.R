331a14bff5e434032e813608e0f9ace56e44b911
peter <petrel.harp@gmail.com>
2014-01-24 14:20:40 -0800

    .

--- explore-data.R
+++ explore-data.R
@@ -25,7 +25,7 @@ shapediff$specimen1 <- factor( shapediff$specimen1, levels=specimens )
 shapediff$specimen2 <- factor( shapediff$specimen2, levels=specimens )
 
 require(ape)
-tree <- read.nexus("consensusTree_10_species.txt")
+# tree <- read.nexus("consensusTree_10_species.txt")
 
 bones$genus <- bones$species
 levels(bones$genus) <- sapply( strsplit( levels(bones$genus), "_" ), "[[", 1 )
@@ -58,7 +58,7 @@ genuscolors <- adjustcolor( rainbow(nlevels(bones$genus)+5)[1:nlevels(bones$genu
 genusadj <- (-.5) + (1/(nlevels(bones$genus)+4))*as.numeric(shapediff$genus1)
 genusadj2 <- (-.5) + (1/(nlevels(bones$genus)+4))*as.numeric(shapediff$genus2)
 
-pdf(file="species-genus-comparisons.pdf",width=10,height=8,pointsize=10)
+pdf(file="figures/species-genus-comparisons.pdf",width=10,height=8,pointsize=10)
 layout(matrix(c(1,2,3,3),nrow=2),widths=c(5,1))
 for (thisbone in c("pelvic","rib")) {
     plot( shape_difference ~ comparison, data=shapediff, subset=(bone1==bone2 & bone1==thisbone), main=thisbone )
@@ -74,7 +74,7 @@ legend("topleft",legend=levels(bones$genus),col=genuscolors,pch=20)
 par(opar)
 dev.off()
 
-pdf(file="species-genus-side-comparisons.pdf",width=10,height=8,pointsize=10)
+pdf(file="figures/species-genus-side-comparisons.pdf",width=10,height=8,pointsize=10)
 layout(matrix(c(1,2,3,3),nrow=2),widths=c(5,1))
 for (thisbone in c("pelvic","rib")) {
     plot( shape_difference ~ comparison, data=shapediff, subset=(bone1==bone2 & bone1==thisbone), main=thisbone )
@@ -110,11 +110,11 @@ if (FALSE) {
 }
 
 
-pdf(file="shapediff-by-phylodist.pdf",width=10,height=8,pointsize=10)
+pdf(file="figures/shapediff-by-phylodist.pdf",width=10,height=8,pointsize=10)
 
 layout(matrix(c(1,2,3,3),nrow=2),widths=c(5,1))
 for (thisbone in c("pelvic","rib")) {
-    with( subset(shapediff,bone1==bone2&bone1==thisbone), plot( shape_difference ~ phylodist, col=genuscolors[genus1] ) )
+    with( subset(shapediff,bone1==bone2&bone1==thisbone), plot( shape_difference ~ phylodist, col=genuscolors[genus1], main=thisbone ) )
 }
 opar <- par(mar=c(5,0,4,0)+.1)
 plot(0,type='n',xaxt='n',yaxt='n',xlab='',ylab='')
@@ -123,7 +123,7 @@ par(opar)
 
 layout(matrix(c(1,2,3,3),nrow=2),widths=c(5,1))
 for (thisbone in c("pelvic","rib")) {
-    with( subset(shapediff,bone1==bone2&bone1==thisbone), plot( shape_difference ~ phylodist, col=genuscolors[genus1], xlim=c(0,.14) ) )
+    with( subset(shapediff,bone1==bone2&bone1==thisbone), plot( shape_difference ~ phylodist, col=genuscolors[genus1], xlim=c(0,.14), main=paste(thisbone,"zoomed in") ) )
 }
 opar <- par(mar=c(5,0,4,0)+.1)
 plot(0,type='n',xaxt='n',yaxt='n',xlab='',ylab='')
